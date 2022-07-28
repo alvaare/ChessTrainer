d4e1b242171e0a9878cdd8e3d1dda5fbc4b98045
Alvaro Arenas <alvaare@gmail.com>
2022-07-28 20:37:12 +0200

    Improve FEN parser

--- src/board.rs
+++ src/board.rs
@@ -2,28 +2,46 @@ use crate::piece::{*};
 use crate::piece::Piece::*;
 use crate::piece::Color::*;
 
-type Coord = (usize, usize);
 const BOARD_SIZE : usize = 8;
+type Coord = (usize, usize);
+
+pub fn from_str_to_coord(s: &str) -> Option<Coord> {
+    if s.len()!=2 {return None;}
+    let mut s = s.bytes();
+    Some(((s.nth(0).unwrap() - 'a' as u8).into(),
+    ((s.nth(1).unwrap() as char).to_digit(10).unwrap() -1) as usize))
+}
+
 
 #[derive(Debug)]
 pub struct Board {
-    board: [[Option<Piece>; BOARD_SIZE]; BOARD_SIZE]
+    position: [[Option<Piece>; BOARD_SIZE]; BOARD_SIZE],
+    turn: Color,
+    castlings: [bool; 4],
+    en_passant: Option<Coord>,
+    halfmove_clock: usize,
+    move_count: usize
 }
 
 impl Board {
     pub fn new() -> Self {
         Board {
-            board: [[None; BOARD_SIZE]; BOARD_SIZE]
+            position: [[None; BOARD_SIZE]; BOARD_SIZE],
+            turn: WHITE,
+            castlings: [true; 4],
+            en_passant: None,
+            halfmove_clock: 0,
+            move_count: 0
         }
     }
 
     fn set(&mut self, coord: Coord, piece: Option<Piece>) {
-        self.board[coord.0][coord.1] = piece;
+        self.position[coord.0][coord.1] = piece;
     }
 
     fn set_line(&mut self, id_line: usize, piece: Option<Piece>) {
         for id_column in 0..BOARD_SIZE {
-            self.board[id_line][id_column] = piece;
+            self.position[id_line][id_column] = piece;
         }
     }
 
@@ -50,27 +68,70 @@ impl Board {
         board
     }
 
-    fn read_rank(&mut self, fen: &[u8], id_rank: usize, id_char: &mut usize) {
+    fn read_rank(&mut self, rank: &str, id_rank: usize) {
         let mut id_col : usize = 0;
-        loop {
-            println!("{}", fen[*id_char] as char);
-            match fen[*id_char] {
+        for c in rank.bytes() {
+            match c {
                 b'/' | b' ' => return,
-                b'1'..=b'8' => id_col += ((fen[*id_char] as char).to_digit(10).unwrap() -1) as usize,
+                b'1'..=b'8' => id_col += ((c as char).to_digit(10).unwrap() -1) as usize,
                 c => self.set((id_rank, id_col), Piece::from_char(&c))
             }
             id_col += 1;
-            *id_char += 1;
         }
     }
 
-    pub fn from_fen(fen: &[u8]) -> Self {
-        let mut board = Board::new();
-        let mut id_char : usize = 0;
-        for id_rank in 1..=8 {
-            board.read_rank(&fen, 8-id_rank, &mut id_char);
-            id_char += 1;
+    fn read_ranks(&mut self, fen: &str) {
+        let ranks = fen.split('/');
+        for (id_rank, rank) in ranks.enumerate() {
+            self.read_rank(rank, 7-id_rank);
         }
+    }
+
+    fn read_turn(&mut self, fen: &str) {
+        self.turn = match fen {
+            "b" => Color::BLACK,
+            "w" => Color::WHITE,
+            _ => panic!("The turn must be b or w")
+        }
+    }
+
+    fn read_castlings(&mut self, fen: &str) {
+        self.castlings = [false; 4];
+        for c in fen.bytes() {
+            match c {
+                b'K' => self.castlings[0] = true,
+                b'Q' => self.castlings[1] = true,
+                b'k' => self.castlings[2] = true,
+                b'q' => self.castlings[3] = true,
+                _ => ()
+            }
+        }
+    }
+
+    fn read_en_passant(&mut self, fen: &str) {
+        self.en_passant = match fen {
+            "-" => None,
+            s => from_str_to_coord(s)
+        }
+    }
+
+    fn read_clock(&mut self, fen: &str) {
+        self.halfmove_clock = fen.parse().unwrap();
+    }
+
+    fn read_move(&mut self, fen: &str) {
+        self.move_count = fen.parse().unwrap();
+    }
+
+    pub fn from_fen(fen: &str) -> Self {
+        let mut board = Board::new();
+        let vec: Vec<&str> = fen.split_whitespace().collect();
+        board.read_ranks(vec[0]);
+        board.read_turn(vec[1]);
+        board.read_castlings(vec[2]);
+        board.read_en_passant(vec[3]);
+        board.read_clock(vec[4]);
+        board.read_move(vec[5]);
         board
     }
 }
